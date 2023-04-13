a8feb233c6ad6b5d20a0bc04732d2e6bd251e5f4
yordisev <yordys12@gmail.com>
2023-04-13 08:15:46 -0500

    listo

--- appconrutas.py
+++ appconrutas.py
@@ -1,9 +1,10 @@
 from flask import Flask
-
+from flask_cors import CORS
 from user_crud import users_bp
 from client_crud import clients_bp
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 app.register_blueprint(users_bp)
 app.register_blueprint(clients_bp)
 
